35f8c560f75608d1877d078e595624631d63a215
Dzianis Hrynko <d.hrynko@godeltech.com>
2021-03-25 10:41:42 +0300

    fix: 2.1 BEM

--- app.js
+++ app.js
@@ -85,11 +85,11 @@ var editTask=function(){
     var editInput=listItem.querySelector('input[type=text]');
     var label=listItem.querySelector("label");
     var editBtn=listItem.querySelector(".edit");
-    var containsClass=listItem.classList.contains("edit-mode");
-    //If class of the parent is .edit-mode
+    var containsClass=listItem.classList.contains("centered-element__li");
+    //If class of the parent is .centered-element__li
     if(containsClass){
 
-        //switch to .edit-mode
+        //switch to .centered-element__li
         //label becomes the inputs value.
         label.innerText=editInput.value;
         editBtn.innerText="Edit";
@@ -98,8 +98,8 @@ var editTask=function(){
         editBtn.innerText="Save";
     }
 
-    //toggle .edit-mode on the parent.
-    listItem.classList.toggle("edit-mode");
+    //toggle .centered-element__li on the parent.
+    listItem.classList.toggle("centered-element__li");
 };
 
 
